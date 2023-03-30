a9ee7ae2bf0a5dcca6b249eafb32de091a01acd0
zhaozz@1996 <128444805+zhaozz1996@users.noreply.github.com>
2023-03-30 08:36:48 +0800

    Add files via upload

--- src/default.ts
+++ src/default.ts
@@ -8,10 +8,8 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 
-- 特感谢 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 由于本站访问量过大，为防止恶意攻击暂时关闭全局key，添加站长微信备注key后使用。
 
-- 本站由 [吖诗啦嘞](https://mp.weixin.qq.com/s?__biz=MzU1Mzg5MDU1OA==&mid=2247483658&idx=1&sn=46f746ce95d0d3293dfd56caf4b5ff82&chksm=fbeaa69bcc9d2f8daf15dace63e5126090094a1babe229df55ddc6dc33be64b1aa5a724e1497#rd) 搭建。
+- 想要部署的朋友，可关注公众号：**吖诗啦程序员**，添加我个人微信获取保姆级部署文档，
 
-- 想要部署的朋友，可关注公众号：**吖诗啦嘞**，添加我个人微信获取保姆级部署文档，
-
-- **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 开头输入 / 或者 空格 触发预设角色指令。
